bdea6307f73aa9b57d58771e9f35160c8e10215b
RoUchiha <swaggsikh@gmail.com>
2022-03-03 23:30:32 -0600

    commit tree 1d19797e559fdcbf2e1b6ac8a7e00e462508e4b4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    let match = array.filter(str => str.toLowerCase() === string.toLowerCase());
+    return match
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter( str => str.startsWith(string))
+}
+
+function matchName(array, string) {
+   return array.filter(str => str.name === string )
+}
